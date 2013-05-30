6682b80b0aac5e0255f7e8f28de0236aa5e67ccb
NicoKiss <879709882@qq.com>
2013-05-31 05:46:29 +0800

    added function int add(int,int)

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+int add(int a,int b)
+{
+	return a+b;
+}
 int main(int argc,char *argv[])
 {
+	int a=1,b=2;
 	printf("Hello \n");
 	printf("Second\n");
+	printf("%d",add(a,b));
 	return 0;
 }
